180fe2b6f2e9860f0dc809cfa020e6b12a57a130
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-04-25 09:39:32 -0700

    Add Camera

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ export default function App() {
   }, []);
 
   const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
